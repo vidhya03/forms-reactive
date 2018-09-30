8584289d84505e5130469a9848bfc53919aab29c
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-09-30 19:11:53 +0530

    Added form control group and access the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email])
+      }),
       gender: new FormControl('male')
     });
   }
